87baa9532eb3c786a10c96cd292043393061fb8a
Knowledgehunt1 <116151857+Knowledgehunt1@users.noreply.github.com>
2025-11-22 11:10:31 +0530

    method overridden successfully   implimented

---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,11 @@
+package com.driver;
+
+public class B extends  A{
+    int n=10;
+   public  String meth()
+    {
+        return "Method is overridden in Extended class B" ;
+    }
+
+
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B b1= new B();
+        String str=b1.meth();
+        System.out.print(str);
+    }
+
+
 }
